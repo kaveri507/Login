35e29974412c80d21907a973272cdddb26aed2fd
kaveri507 <kaveri.veluru@gmail.com>
2019-06-22 15:53:42 +0530

    first commit

---
+++ Login/src/com/app/controller/MyServlet.java
@@ -0,0 +1,42 @@
+package com.app.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/loginAction")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name=request.getParameter("userName");
+		String pass=request.getParameter("password");
+		if(name.equals("kaveri")&&pass.equals("123"))
+		{
+			response.sendRedirect("success.jsp");
+		}
+		else
+		{
+			response.sendRedirect("error.jsp");
+		}
+	}
+
+}
